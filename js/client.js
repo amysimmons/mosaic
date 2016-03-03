f4dc168e3218493abc243df08bb9a354acfae1ba
amysimmons <amy__simmons@hotmail.com>
2016-03-03 20:42:55 +1100

    image loading into canvas on page

--- js/client.js
+++ js/client.js
@@ -1,1 +1,38 @@
-// Edit me.
+function App() {
+	this.file;
+	this.image;
+	this.grid = [];
+
+	document.getElementById("uploadInput").onchange = function() {
+		loadImage(this);
+	};
+}
+
+function loadImage(input){
+	App.file = input.files[0];
+	var canvas = document.getElementById('photo-mosaic-canvas');
+	var ctx = canvas.getContext('2d');
+   	var reader = new FileReader();
+
+    reader.onload = function(e) {
+    	var img = new Image();
+    	img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+        App.image = img;
+    };
+
+    reader.readAsDataURL(App.file);
+}
+
+function cropImage(){
+
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var app = new App();
+});
+	
