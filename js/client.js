ef2f7c174f3e045e64a67c5699d69b30012c11be
amysimmons <amy__simmons@hotmail.com>
2016-03-07 21:35:19 +1100

    refactoring

--- js/client.js
+++ js/client.js
@@ -1,17 +1,16 @@
-function App() {
-	this.file;
-	this.image;
+function App(){
+	this.photo = null;
 	this.grid = [];
 
-	document.getElementById("uploadInput").onchange = function() {
+	document.getElementById("uploadInput").onchange = function(){
 		loadImage(this);
 	};
 
 	document.getElementById("toggleMosaicBtn").onclick = function(){
-	    if (this.innerHTML=="Show mosaic") {
-	    	this.innerHTML = "Show original";
-	    	generateMosaic();
-	    }
+		if (this.innerHTML == "Show mosaic") {
+			this.innerHTML = "Show original";
+			generateMosaic();
+		}
 		else {
 			this.innerHTML = "Show mosaic";
 			renderOriginal();
@@ -19,25 +18,26 @@ function App() {
 	}
 }
 
+//loads the photo onto a canvas and renders the toggle mosaic/photo button
 function loadImage(input){
-	App.file = input.files[0];
+	var file = input.files[0];
 	var canvas = document.getElementById('photo-canvas');
 	var ctx = canvas.getContext('2d');
-   	var reader = new FileReader();
+	var reader = new FileReader();
 
-    reader.onload = function(e) {
-    	var img = new Image();
-    	img.onload = function(){
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-        }
-        img.src = event.target.result;
-        App.image = img;
-    };
+	reader.onload = function(e){
+		var img = new Image();
+		img.onload = function(){
+			canvas.width = img.width;
+			canvas.height = img.height;
+			ctx.drawImage(img, 0, 0);
+		}
+		img.src = event.target.result;
+		App.photo = img;
+	};
 
-    reader.readAsDataURL(App.file);
-    showToggleButton();
+	reader.readAsDataURL(file);
+	showToggleButton();
 }
 
 function showToggleButton(){
@@ -45,37 +45,33 @@ function showToggleButton(){
 }
 
 function generateMosaic(){
-	App.grid = sliceImage();
+	App.grid = slicePhoto();
 	calculateAverageColors();
-	fetchTiles();
+	generateMosaicPieces();
 	renderMosaic();
 }
 
-function sliceImage(){
+//slices up the uploaded photo into tiles
+function slicePhoto(){
 	var canvas = document.getElementById('photo-canvas');
 	var ctx = canvas.getContext('2d');
 	
+	var grid = [];
+	var id = 0;
 	var sx = 0;
 	var sy = 0;
 	var sw = TILE_WIDTH;
 	var sh = TILE_HEIGHT;
 	
-	var id = 0;
-	var grid = [];
-
 	while(sy <= canvas.height){
-		var row = {className: "row hide", tiles: []};
+		var row = {tiles: [], className: "row hide"};
 		while (sx <= canvas.width){
 			var imageData = ctx.getImageData(sx, sy, sw, sh);
 			var tile = {
 				id: id,
-				imageData: imageData,
+				photoData: imageData,
 				averageColor: null,
-				image: null,
-				sx: sx,
-				sy: sy,
-				sw: sw,
-				sh: sh,
+				mosaicPiece: null,
 				loaded: false
 			}
 			row.tiles.push(tile);
@@ -89,160 +85,132 @@ function sliceImage(){
 	return grid;
 }
 
+//populates an average colour for each photo tile in the grid
 function calculateAverageColors(){
-	var grid = App.grid;
-
-	for (var i = 0; i < grid.length; i++) {
-		
-		var row = grid[i].tiles;
-
+	for (var i = 0; i < App.grid.length; i++) {
+		var row = App.grid[i].tiles;
 		for (var j = 0; j < row.length; j++) {
-
 			var tile = row[j];
-
+			//sets up arrays to calculate the average red, green, blue and alpha for each photo tile
 			var r = [];
 			var g = [];
 			var b = [];
-			var a = [];
-
-			for (var k = 0; k < tile.imageData.data.length; k += 4) {
-				
-				r.push(tile.imageData.data[k]);
-				g.push(tile.imageData.data[k + 1]);
-				b.push(tile.imageData.data[k + 2]);
-				a.push(tile.imageData.data[k + 3]);
-
+			// tile.photoData.data is a 1024 length array, with every four values being an r, g, b then a
+			// this is why the loop increments k by 4 and asssigns the values internally 
+			// 'a' values are skipped because we only need rgb
+			for (var k = 0; k < tile.photoData.data.length; k += 4) {
+				r.push(tile.photoData.data[k]);
+				g.push(tile.photoData.data[k + 1]);
+				b.push(tile.photoData.data[k + 2]);
 			};
 
-			var rSum = r.reduce(function(a, b) { return a + b; });
-			var rAvg = Math.floor(rSum/r.length);
-
-			var gSum = g.reduce(function(a, b) { return a + b; });
-			var gAvg = Math.floor(gSum/g.length);
-
-			var bSum = b.reduce(function(a, b) { return a + b; });
-			var bAvg = Math.floor(bSum/b.length);
-
-			var aSum = a.reduce(function(a, b) { return a + b; });
-			var aAvg = Math.floor(aSum/a.length);
+			function getAverage(values){
+				var sum = values.reduce(function(a, b) { return a + b; });
+				return Math.floor(sum/values.length)
+			}
 
 			function componentToHex(c) {
-			    var hex = c.toString(16);
-			    return hex.length == 1 ? "0" + hex : hex;
+				var hex = c.toString(16);
+				return hex.length == 1 ? "0" + hex : hex;
 			}
 
 			function rgbToHex(r, g, b) {
-			    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+				return componentToHex(r) + componentToHex(g) + componentToHex(b);
 			}
 
-			tile.averageColor = rgbToHex(rAvg, gAvg, bAvg);
-
+			tile.averageColor = rgbToHex(getAverage(r), getAverage(g), getAverage(b));
 		};
-
 	};
-	
 }
 
-function fetchTiles(){
+//creates a mosaic piece for each photo tile in the grid
+function generateMosaicPieces(){
 	for (var i = 0; i < App.grid.length; i++) {
-		
 		var row = App.grid[i];
-
 		row.tiles.map(function(tile){
-			var hex = tile.averageColor;
-			var src = `/color/${hex}`;
-			var image = document.createElement('img');
-			image.src = src;
-			image.id=tile.id;
-			image.onload=function(){
-				image.className="loaded"; 
-				tile.loaded=true; 
-				console.log('heya loaded')
+			var src = `/color/${tile.averageColor}`;
+			var img = document.createElement('img');
+			img.src = src;
+			img.id = tile.id;
+
+			img.onload=function(){
+				img.className = "loaded"; 
+				tile.loaded = true; 
 				checkIfRowLoaded(tile);
 			}
-			tile.image = image;
+
+			tile.mosaicPiece = img;
 		});
 	};
 }
 
+//renders the mosaic in the browser
 function renderMosaic(){
-
 	var container = document.getElementById('mosaic-container');
+	for (var i = 0; i < App.grid.length; i++) {	
 
-	for (var i = 0; i < App.grid.length; i++) {
-			
 		var row = App.grid[i];
-		var tiles = App.grid[i].tiles
-		var div = document.createElement('div')
+		var tiles = App.grid[i].tiles;
+
+		var div = document.createElement('div');
 		div.className = row.className;
 	
 		for (var j = 0; j < tiles.length; j++) {
 			var tile = tiles[j];
-			div.appendChild(tile.image);
+			div.appendChild(tile.mosaicPiece);
 		};
 	
 		container.appendChild(div);	
-
 	};
-
  }
 
- function checkIfRowLoaded(tile){
-
-	function getRow(tile){
-
-		var rows = document.getElementsByClassName('row');
-
-		for (var i = 0; i < App.grid.length; i++) {
-			var row = App.grid[i];
-			var tileIds = row.tiles.map(function(tile){return tile.id});
-
-			if(tileIds.indexOf(tile.id) >= 0){
-				console.log('row returned')
-				return row;
-			}
-
-		};
-	}
-
-	function getDomRow(tile){
-
-		var rows = document.getElementsByClassName('row');
-
-		var domRow = null;
-
-		for (var i = 0; i < rows.length; i++) {
-			var row = rows[i];
-			for (var x = 0; x < row.childNodes.length; x++){
-			    if (row.childNodes[x].id == tile.id) {
-			    	domRow = row;
-			    }      
-			}
-		}
-		return domRow;
-	}
-
+//checks if all mosaicPieces in a row are loaded before showing the row
+function checkIfRowLoaded(tile){
 	var row = getRow(tile);
 	var domRow = getDomRow(tile);
 
 	var loadValues = row.tiles.map(function(tile){return tile.loaded});
 
 	if (loadValues.indexOf(false) == [-1]){
-		console.log('show class here1!!')
 		row.className = "row show";
-		if (domRow != undefined) {
-			domRow.className = "row show";
-		}
- 	}  
+		domRow.className = "row show";
+	}
+}
 
+function getRow(tile){
+	var rows = document.getElementsByClassName('row');
+
+	for (var i = 0; i < App.grid.length; i++) {
+		var row = App.grid[i];
+		var tileIds = row.tiles.map(function(tile){return tile.id});
+
+		if(tileIds.indexOf(tile.id) >= 0){
+			console.log('row returned')
+			return row;
+		}
+	};
+}
+
+function getDomRow(tile){
+	var rows = document.getElementsByClassName('row');
+
+	var domRow = null;
+
+	for (var i = 0; i < rows.length; i++) {
+		var row = rows[i];
+		for (var x = 0; x < row.childNodes.length; x++){
+			if (row.childNodes[x].id == tile.id) {
+			domRow = row;
+			}
+		}
+	}
+	return domRow;
 }
 
 function renderOriginal(){
-
 
 }
 
 document.addEventListener("DOMContentLoaded", function(event) { 
 	var app = new App();
 });
-	
